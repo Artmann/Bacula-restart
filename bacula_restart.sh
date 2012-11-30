bb2969962701ad0ae20b92062e208cc0a906cef2
Christoffer Artmann <christoffer.artmann@glesys.se>
2012-11-30 13:25:03 +0100

    Sharks

--- bacula_restart.sh
+++ bacula_restart.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+if [ "$1" = "-shark" ]
+then
+	echo "It's freaking sharks with lazers attached to their heads!"
+fi
+
 /etc/init.d/bacula-director restart
 /etc/init.d/bacula-fd restart
 /etc/init.d/bacula-sd restart
